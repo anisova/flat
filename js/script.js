b2fd05b87ad38d56d736d9ad1a4271e99eb5b6f2
anisova <39698859+anisova@users.noreply.github.com>
2022-02-22 20:28:36 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,9 +13,36 @@ function ham() {
   });
   menu.addEventListener("click", function (e) {
     let target = e.target;
-    if (target.className === "menu__link") {
+    if (target.className === "menu-m__link") {
       toggleMenu();
     }
   });
 }
 ham();
+
+let select = function () {
+  let selectHeader = document.querySelectorAll(".select__header");
+  let selectItem = document.querySelectorAll(".select__item");
+  selectHeader.forEach((item) => {
+    item.addEventListener("click", selectToggle);
+  });
+
+  selectItem.forEach((item) => {
+    item.addEventListener("click", selectChoose);
+  });
+  function selectToggle() {
+    // let select = document.querySelectorAll(".select");
+    // select.forEach((item) => {
+    //   item.classList.remove("is-active");
+    // });
+    this.parentElement.classList.toggle("is-active");
+  }
+  function selectChoose() {
+    let text = this.innerText,
+      select = this.closest(".select"),
+      currentText = select.querySelector(".select__current");
+    currentText.innerText = text;
+    select.classList.remove("is-active");
+  }
+};
+select();
